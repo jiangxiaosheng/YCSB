06f0cc3e3157de03c1535644f907d08198a6817a
Haoyu Li <haoyu.li@columbia.edu>
2021-02-08 05:07:28 -0700

    Add build.sh for YCSB and build-and-run.sh for replicator

---
+++ replicator/build-and-run.sh
@@ -0,0 +1,3 @@
+javac -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Replicator.java
+javac -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Server.java
+java -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Server
