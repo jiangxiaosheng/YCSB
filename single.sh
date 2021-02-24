b6da9bcd3898f4e777c8fb87269b0c563de87e89
cc4351 <cc4351@node0.cc4351-qv92229.e6897-pg0.utah.cloudlab.us>
2021-02-23 19:13:00 -0700

    modify script to run all workloads

--- single.sh
+++ single.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for char in b c d e f
+for char in a b c d e f 
 do 
 	echo pin$char
-	for i in {1..3}
+	for i in {1..1}
 	do
-		./bin/ycsb load rocksdb -s -P workloads/workload$char -p rocksdb.dir=/tmp/ycsb-rocksdb-data >> out$char.txt
-		./bin/ycsb run rocksdb -s -P workloads/workload$char -p rocksdb.dir=/tmp/ycsb-rocksdb-data >> out$char.txt
+		./bin/ycsb load rocksdb -s -P workloads/workload$char -p rocksdb.dir=/tmp/ycsb-rocksdb-data -threads 8 >> out$char.txt
+		./bin/ycsb run rocksdb -s -P workloads/workload$char -p rocksdb.dir=/tmp/ycsb-rocksdb-data -threads 8 >> out$char.txt
 	done
 	rm -rf /tmp/ycsb-rocksdb-data/
 done
