1246faa7e6aa6cb3b4683cfabc70061a9b70725b
cc4351 <cc4351@columbia.edu>
2021-03-01 17:22:32 +0800

    replicator sketch;forwarding callable TBD

--- replicator/Replicator.java
+++ replicator/Replicator.java
@@ -20,81 +20,64 @@ import net.jcip.annotations.GuardedBy;
 public class Replicator {
 
   private ServerSocket servSock;
-  private String role;
-  private ExecutorService executor;
+  private ServerSocket replySock;
 
-  @GuardedBy("Replicator.class") private static RocksDB rocksDb = null;
-  @GuardedBy("Replicator.class") private static int references = 0;
-  @GuardedBy("Replicator.class") private static Path rocksDbDir = null;
-  @GuardedBy("Replicator.class") private static Path optionsFile = null;
-  @GuardedBy("Replicator.class") private static RocksObject dbOptions = null;
+  /* 
+   recv: responsible for receiving request from client and sending reply back
+   shardClient: 
+   - responsible for forwarding the request downstream
+   - TODO: use customized threadfactory for these threadpool
+   - TODO: write a threadpool that executes callable to take in responses from tail
+      - listen on same port for responses from all three shards
+      - once a match with sequence number, send the response back to client
 
-  static final String PROPERTY_ROCKSDB_DIR = "rocksdb.dir";
-  static final String PROPERTY_ROCKSDB_OPTIONS_FILE = "rocksdb.optionsfile";
-  private static final String COLUMN_FAMILY_NAMES_FILENAME = "CF_NAMES";
+  */
+  private ExecutorService recv;
+  private List<ExecutorService> shardClient;
 
-  private static final ConcurrentMap<String, ColumnFamily> COLUMN_FAMILIES = new ConcurrentHashMap<>();
-  private static final ConcurrentMap<String, Lock> COLUMN_FAMILY_LOCKS = new ConcurrentHashMap<>();
-
-  //private static final Logger LOGGER = LoggerFactory.getLogger(Replicator.class);
-
-  public void init(String role) throws DBException {
-    this.role = role;
-    this.executor = Executors.newCachedThreadPool();
+  public void init(int shard, int threads) throws DBException {
+    this.recv = Executors.newCachedThreadPool();
     //this.executor = Executors.newFixedThreadPool(1000);
-
-    synchronized(Replicator.class) {
-      if(rocksDb == null) {
-        rocksDbDir = Paths.get(PROPERTY_ROCKSDB_DIR);
-        //LOGGER.info("RocksDB data dir: " + rocksDbDir);
-
-        //String optionsFileString = PROPERTY_ROCKSDB_OPTIONS_FILE;
-        String optionsFileString = null;
-        if (optionsFileString != null) {
-          optionsFile = Paths.get(optionsFileString);
-          //LOGGER.info("RocksDB options file: " + optionsFile);
-        }
-
-        try {
-          if (optionsFile != null) {
-            rocksDb = initRocksDBWithOptionsFile();
-          } else {
-            rocksDb = initRocksDB();
-          }
-        } catch (final IOException | RocksDBException e) {
-          throw new DBException(e);
-        }
-      }
-
-      references++;
+    this.shardClient = new ArrayList<>();
+    for(int i = 0; i < shard; i++) {
+      tmp = Executors.newFixedThreadPool(threads);
+      this.shardClient.add(tmp);
     }
   }
 
-  public void start(int port) throws IOException {
+  public void start(int clientPort, int replyPort) throws IOException {
     //start the server socket
     try {
-      servSock = new ServerSocket(port, 1000);
+      this.servSock = new ServerSocket(clientPort, 1000);
+      this.replySock = new ServerSocket(replyPort, 1000);
     } catch (IOException e) {
       e.printStackTrace();
     }
     //handle requests
     while (true) {
-      this.executor.execute(new ClientHandler(servSock.accept()));
+      this.recv.execute(new ClientHandler(servSock.accept()));
     }
   }
 
-  public void stop() {
+  public void shutdownService(ExecutorService executor) {
     executor.shutdown();
     try {
-        if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-            executor.shutdownNow();
-        } 
-    } catch (InterruptedException e) {
+      if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
         executor.shutdownNow();
+      } 
+    } catch (InterruptedException e) {
+      executor.shutdownNow();
     }
+  }
+  public void stop() {
+    for(ExecutorService exe: shardClient) {
+      shutdownService(exe);
+    }
+    shutdownService(recv);
     //close the socket
     try {
       servSock.close();
+      replySock.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -112,7 +95,6 @@ public class Replicator {
 
     public void run() {
       try {
-        outstream = new ObjectOutputStream(clientSock.getOutputStream());
         in = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
         String str;
         while((str = in.readLine()) != null) {
@@ -122,372 +104,25 @@ public class Replicator {
           } else if (str.length() < 7) {
             System.out.println(str + " is not a valid operation");
           } else {
-            //TODO: error handling
-            str = "{"+ str.split("\\{", 2)[1];
-            //System.out.println(str);
             Gson gson = new Gson();
-            //de-serialize json string and handle operation
+            //de-serialize json string and forward operations
             try {
               ReplicatorOp op = gson.fromJson(str, ReplicatorOp.class);
-              // System.out.println("op received: " + op.getOp());
-              Reply reply = opHandler(op);
-              //add line break to read entries line by line
-              String json = gson.toJson(reply) + "\n";
-              //System.out.println(json);
-              outstream.writeObject(json);
-            } catch (Exception e) {
-              System.out.println("seg: " + str);
+              //TODO: some load-distribution algo here to distribute request to shard heads
+              //current implementation default to the first executor in list
+              Replicator.shardClient.get(0).execute();
+
+            }catch (Exception e) {
+              System.err.println("replicator deserialization failure")
               e.printStackTrace();
             }
           }
         }
         in.close();
-        // instream.close();
-        outstream.close();
         clientSock.close();
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
-  }
-
-  private Reply opHandler(ReplicatorOp op) {
-
-    String table = op.getTable();
-    String key = op.getKey();
-    //byte[] ret = new byte[0];
-		Reply reply = new Reply();
-    reply.setOp(op.getOp());
-
-    try {
-      if (!COLUMN_FAMILIES.containsKey(table)) {
-        createColumnFamily(table);
-      }
-
-      final ColumnFamilyHandle cf = COLUMN_FAMILIES.get(table).getHandle();
-      //System.out.println(op.getValues().length);
-      
-      //process different ops
-      switch (op.getOp()) {
-        case "insert":
-          rocksDb.put(cf, key.getBytes(UTF_8), op.getValues());
-          //TODO: insert comm with two secondaries here
-          reply.setStatus(site.ycsb.Status.OK);
-          break;
-        case "read":
-          final byte[] val = rocksDb.get(cf, key.getBytes(UTF_8));
-          if(val == null) {
-            reply.setStatus(site.ycsb.Status.NOT_FOUND);
-            System.out.println("read - status: not found");
-          } else {
-            reply.setValues(val);
-            reply.setStatus(site.ycsb.Status.OK);
-          }
-          break;
-        //TODO: need to expand the ReplicatorOp class fields
-				//implementation of scan could be delayed -> dummy impl now
-        case "scan":
-          reply.setStatus(site.ycsb.Status.OK);
-          break;
-          /*
-          try(final RocksIterator iterator = rocksDb.newIterator(cf)) {
-            int iterations = 0;
-            for (iterator.seek(startkey.getBytes(UTF_8)); iterator.isValid() && iterations < recordcount;
-                iterator.next()) {
-              final HashMap<String, ByteIterator> values = new HashMap<>();
-              deserializeValues(iterator.value(), fields, values);
-              result.add(values);
-              iterations++;
-            }
-          }*/
-        case "update":
-          final Map<String, ByteIterator> result = new HashMap<>();
-          final Map<String, ByteIterator> update = new HashMap<>();
-          final byte[] currentValues = rocksDb.get(cf, key.getBytes(UTF_8));
-          if(currentValues == null) {
-            reply.setStatus(site.ycsb.Status.NOT_FOUND);
-            System.out.println("update - status: not found");
-            break;
-          }
-          deserializeValues(currentValues, null, result);
-          deserializeValues(op.getValues(), null, update);
-          //update
-          result.putAll(update);
-          //store
-          rocksDb.put(cf, key.getBytes(UTF_8), serializeValues(result));
-          reply.setStatus(site.ycsb.Status.OK);
-          break;
-        case "delete":
-          rocksDb.delete(cf, key.getBytes(UTF_8));
-          reply.setStatus(site.ycsb.Status.OK);
-        default:
-          break;
-      }
-      //System.out.println("status: ok");
-    } catch(final IOException | RocksDBException e) {
-      //LOGGER.error(e.getMessage(), e);
-      reply.setStatus(site.ycsb.Status.ERROR);
-      System.out.println("status: error");
-      e.printStackTrace();
-    }
-    return reply;
-  }
-
-  private RocksDB initRocksDB() throws IOException, RocksDBException {
-    if(!Files.exists(rocksDbDir)) {
-      Files.createDirectories(rocksDbDir);
-    }
-
-    final List<String> cfNames = loadColumnFamilyNames();
-    final List<ColumnFamilyOptions> cfOptionss = new ArrayList<>();
-    final List<ColumnFamilyDescriptor> cfDescriptors = new ArrayList<>();
-
-    for(final String cfName : cfNames) {
-      final ColumnFamilyOptions cfOptions = new ColumnFamilyOptions()
-          .optimizeLevelStyleCompaction();
-      final ColumnFamilyDescriptor cfDescriptor = new ColumnFamilyDescriptor(
-          cfName.getBytes(UTF_8),
-          cfOptions
-      );
-      cfOptionss.add(cfOptions);
-      cfDescriptors.add(cfDescriptor);
-    }
-
-    final int rocksThreads = Runtime.getRuntime().availableProcessors() * 2;
-
-    if(cfDescriptors.isEmpty()) {
-      final Options options = new Options()
-          .optimizeLevelStyleCompaction()
-          .setCreateIfMissing(true)
-          .setCreateMissingColumnFamilies(true)
-          .setIncreaseParallelism(rocksThreads)
-          .setMaxBackgroundCompactions(rocksThreads)
-          .setInfoLogLevel(InfoLogLevel.INFO_LEVEL);
-      dbOptions = options;
-      return RocksDB.open(options, rocksDbDir.toAbsolutePath().toString());
-    } else {
-      final DBOptions options = new DBOptions()
-          .setCreateIfMissing(true)
-          .setCreateMissingColumnFamilies(true)
-          .setIncreaseParallelism(rocksThreads)
-          .setMaxBackgroundCompactions(rocksThreads)
-          .setInfoLogLevel(InfoLogLevel.INFO_LEVEL);
-      dbOptions = options;
-
-      final List<ColumnFamilyHandle> cfHandles = new ArrayList<>();
-      final RocksDB db = RocksDB.open(options, rocksDbDir.toAbsolutePath().toString(), cfDescriptors, cfHandles);
-      for(int i = 0; i < cfNames.size(); i++) {
-        COLUMN_FAMILIES.put(cfNames.get(i), new ColumnFamily(cfHandles.get(i), cfOptionss.get(i)));
-      }
-      return db;
-    }
-  }
-
-  /**
-   * Initializes and opens the RocksDB database.
-   *
-   * Should only be called with a {@code synchronized(Replicator.class)` block}.
-   *
-   * @return The initialized and open RocksDB instance.
-   */
-  private RocksDB initRocksDBWithOptionsFile() throws IOException, RocksDBException {
-    if(!Files.exists(rocksDbDir)) {
-      Files.createDirectories(rocksDbDir);
-    }
-
-    final DBOptions options = new DBOptions();
-    final List<ColumnFamilyDescriptor> cfDescriptors = new ArrayList<>();
-    final List<ColumnFamilyHandle> cfHandles = new ArrayList<>();
-
-    RocksDB.loadLibrary();
-    OptionsUtil.loadOptionsFromFile(optionsFile.toAbsolutePath().toString(), Env.getDefault(), options, cfDescriptors);
-    dbOptions = options;
-
-    final RocksDB db = RocksDB.open(options, rocksDbDir.toAbsolutePath().toString(), cfDescriptors, cfHandles);
-
-    for(int i = 0; i < cfDescriptors.size(); i++) {
-      String cfName = new String(cfDescriptors.get(i).getName());
-      final ColumnFamilyHandle cfHandle = cfHandles.get(i);
-      final ColumnFamilyOptions cfOptions = cfDescriptors.get(i).getOptions();
-
-      COLUMN_FAMILIES.put(cfName, new ColumnFamily(cfHandle, cfOptions));
-    }
-
-    return db;
-  }
-
-  public void cleanup() throws DBException {
-    //super.cleanup();
-    synchronized (Replicator.class) {
-      try {
-        if (references == 1) {
-          for (final ColumnFamily cf : COLUMN_FAMILIES.values()) {
-            cf.getHandle().close();
-          }
-
-          rocksDb.close();
-          rocksDb = null;
-
-          dbOptions.close();
-          dbOptions = null;
-
-          for (final ColumnFamily cf : COLUMN_FAMILIES.values()) {
-            cf.getOptions().close();
-          }
-          saveColumnFamilyNames();
-          COLUMN_FAMILIES.clear();
-
-          rocksDbDir = null;
-        }
-
-      } catch (final IOException e) {
-        throw new DBException(e);
-      } finally {
-        references--;
-      }
-    }
-  }
-
-  private Map<String, ByteIterator> deserializeValues(final byte[] values, final Set<String> fields,
-      final Map<String, ByteIterator> result) {
-    final ByteBuffer buf = ByteBuffer.allocate(4);
-
-    int offset = 0;
-    while(offset < values.length) {
-      buf.put(values, offset, 4);
-      buf.flip();
-      final int keyLen = buf.getInt();
-      buf.clear();
-      offset += 4;
-
-      final String key = new String(values, offset, keyLen);
-      offset += keyLen;
-
-      buf.put(values, offset, 4);
-      buf.flip();
-      final int valueLen = buf.getInt();
-      buf.clear();
-      offset += 4;
-
-      if(fields == null || fields.contains(key)) {
-        result.put(key, new ByteArrayByteIterator(values, offset, valueLen));
-      }
-
-      offset += valueLen;
-    }
-
-    return result;
-  }
-
-  private byte[] serializeValues(final Map<String, ByteIterator> values) throws IOException {
-    try(final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-      final ByteBuffer buf = ByteBuffer.allocate(4);
-
-      for(final Map.Entry<String, ByteIterator> value : values.entrySet()) {
-        final byte[] keyBytes = value.getKey().getBytes(UTF_8);
-        final byte[] valueBytes = value.getValue().toArray();
-
-        buf.putInt(keyBytes.length);
-        baos.write(buf.array());
-        baos.write(keyBytes);
-
-        buf.clear();
-
-        buf.putInt(valueBytes.length);
-        baos.write(buf.array());
-        baos.write(valueBytes);
-
-        buf.clear();
-      }
-      return baos.toByteArray();
-    }
-  }
-
-  private void createColumnFamily(final String name) throws RocksDBException {
-    COLUMN_FAMILY_LOCKS.putIfAbsent(name, new ReentrantLock());
-
-    final Lock l = COLUMN_FAMILY_LOCKS.get(name);
-    l.lock();
-    try {
-      if(!COLUMN_FAMILIES.containsKey(name)) {
-        final ColumnFamilyOptions cfOptions;
-
-        if (optionsFile != null) {
-          // RocksDB requires all options files to include options for the "default" column family;
-          // apply those options to this column family
-          cfOptions = getDefaultColumnFamilyOptions(name);
-        } else {
-          cfOptions = new ColumnFamilyOptions().optimizeLevelStyleCompaction();
-        }
-
-        final ColumnFamilyHandle cfHandle = rocksDb.createColumnFamily(
-            new ColumnFamilyDescriptor(name.getBytes(UTF_8), cfOptions)
-        );
-        COLUMN_FAMILIES.put(name, new ColumnFamily(cfHandle, cfOptions));
-      }
-    } finally {
-      l.unlock();
-    }
-  }
-
-  private void saveColumnFamilyNames() throws IOException {
-    final Path file = rocksDbDir.resolve(COLUMN_FAMILY_NAMES_FILENAME);
-    try(final PrintWriter writer = new PrintWriter(Files.newBufferedWriter(file, UTF_8))) {
-      writer.println(new String(RocksDB.DEFAULT_COLUMN_FAMILY, UTF_8));
-      for(final String cfName : COLUMN_FAMILIES.keySet()) {
-        writer.println(cfName);
-      }
-    }
-  }
-
-  private List<String> loadColumnFamilyNames() throws IOException {
-    final List<String> cfNames = new ArrayList<>();
-    final Path file = rocksDbDir.resolve(COLUMN_FAMILY_NAMES_FILENAME);
-    if(Files.exists(file)) {
-      try (final LineNumberReader reader =
-               new LineNumberReader(Files.newBufferedReader(file, UTF_8))) {
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-          cfNames.add(line);
-        }
-      }
-    }
-    return cfNames;
-  }
-
-  private ColumnFamilyOptions getDefaultColumnFamilyOptions(final String destinationCfName) {
-    final ColumnFamilyOptions cfOptions;
-
-    if (COLUMN_FAMILIES.containsKey("default")) {
-      //LOGGER.warn("no column family options for \"" + destinationCfName + "\" " +
-                  //"in options file - using options from \"default\"");
-      cfOptions = COLUMN_FAMILIES.get("default").getOptions();
-    } else {
-      //LOGGER.warn("no column family options for either \"" + destinationCfName + "\" or " +
-                  //"\"default\" in options file - initializing with empty configuration");
-      cfOptions = new ColumnFamilyOptions();
-    }
-    //LOGGER.warn("Add a CFOptions section for \"" + destinationCfName + "\" to the options file, " +
-                //"or subsequent runs on this DB will fail.");
-
-    return cfOptions;
-  }
-
-  private static final class ColumnFamily {
-    private final ColumnFamilyHandle handle;
-    private final ColumnFamilyOptions options;
-
-    private ColumnFamily(final ColumnFamilyHandle handle, final ColumnFamilyOptions options) {
-      this.handle = handle;
-      this.options = options;
-    }
-
-    public ColumnFamilyHandle getHandle() {
-      return handle;
-    }
-
-    public ColumnFamilyOptions getOptions() {
-      return options;
-    }
-  }
+  }  
 }
