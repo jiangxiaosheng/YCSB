e13035ffb730d3126ab48664b45cddad5a2744a1
cc4351 <cc4351@columbia.edu>
2021-03-01 21:33:08 +0800

    Replicator with chain replication impl

--- replicator/Replicator.java
+++ replicator/Replicator.java
@@ -10,7 +10,10 @@ import com.google.gson.*;
 public class Replicator {
 
   private ServerSocket servSock;
-  private ServerSocket replySock;
+  private static int seq; //global seq number for operations
+  private static Map<Integer, Socket> waiting; //TODO: modify this to concurrentHashMap
+  private Thread replyT;
+  private Feedback replyRun;
 
   /* 
    recv: responsible for receiving request from client and sending reply back
@@ -23,7 +26,7 @@ public class Replicator {
 
   */
   private ExecutorService recv;
-  private List<ExecutorService> shardClient;
+  private static List<ExecutorService> shardClient;
 
   public void init(Map<String, Integer> shardHeads, int threads) throws DBException {
     this.recv = Executors.newCachedThreadPool();
@@ -32,18 +35,28 @@ public class Replicator {
     // create thread pool executors for different shard heads
     this.shardClient = new ArrayList<>();
     shardHeads.forEach((dest, port) -> 
-      this.shardClient.add(Executors.newFixedThreadPool(threads, new MyFactory(dest, port)))
+      this.shardClient.add(Executors.newFixedThreadPool(threads, new site.ycsb.MyFactory(dest, port)))
     );
+    synchronized(Replicator.class) {
+      this.waiting = new HashMap<>();
+      this.seq = 0;
+    }
+
   }
 
   public void start(int clientPort, int replyPort) throws IOException {
     //start the server socket
     try {
       this.servSock = new ServerSocket(clientPort, 1000);
-      this.replySock = new ServerSocket(replyPort, 1000);
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    // start the listening thread that send replies back to client
+    this.replyRun = new Feedback(replyPort);
+    this.replyT = new Thread(this.replyRun);
+    this.replyT.start();
+
     //handle requests
     while (true) {
       this.recv.execute(new ClientHandler(servSock.accept()));
@@ -60,26 +73,35 @@ public class Replicator {
       executor.shutdownNow();
     }
   }
+
   public void stop() {
+    shutdownService(recv);
     for(ExecutorService exe: shardClient) {
       shutdownService(exe);
     }
-    shutdownService(recv);
-    //close the socket
     try {
+      this.replyRun.terminate();
+      this.replyT.join();
+      //close the socket
       servSock.close();
-      replySock.close();
-    } catch (IOException e) {
+    } catch (IOException | InterruptedException e) {
       e.printStackTrace();
     }
     //cleanup the db
   }
 
   public static void main(String[] args) {
-    Map<Integer, String> shardHeads = new HashMap<>();
+    Map<String, Integer> shardHeads = new HashMap<>();
     //note that each shard head has to have a unique ip
     shardHeads.put("127.0.0.1", 2345);
-
+    Replicator replicator = new Replicator();
+    try {
+      replicator.init(shardHeads, 200);
+      replicator.start(1234, 9876);
+    } catch (DBException | IOException e) {
+      e.printStackTrace();
+    }
+    replicator.stop();
   }
 
   private class ClientHandler implements Runnable {
@@ -108,16 +130,29 @@ public class Replicator {
               ReplicatorOp op = gson.fromJson(str, ReplicatorOp.class);
               //TODO: some load-distribution algo here to distribute request to shard heads
               //current implementation default to the first executor in list
-              Replicator.shardClient.get(0).execute(new Forward(op));
-              
-            }catch (Exception e) {
-              System.err.println("replicator deserialization failure")
+
+              // add sequence number and forward to shard head
+              //TODO: waiting should be changed into concurrentHashMap
+              synchronized(waiting) {
+                Replicator.waiting.put(Replicator.seq, this.clientSock);
+              }
+              synchronized(Replicator.class) {
+                op.setSeq(Replicator.seq++);
+              }
+
+              str = gson.toJson(op) + "\n";
+              //TODO: think more about sync mechanism
+              synchronized(Replicator.shardClient) {
+                Replicator.shardClient.get(0).execute(new Forward(str));
+              }
+            } catch (Exception e) {
+              System.err.println("replicator deserialization failure");
               e.printStackTrace();
             }
           }
         }
         in.close();
-        clientSock.close();
+        // clientSock.close();
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -133,8 +168,89 @@ public class Replicator {
 
     public void run() {
       ObjectOutputStream out = ((MyThread)Thread.currentThread()).getOutStream();
-      out.writeObject(this.op);
+      try {
+        out.writeObject(this.op);
+      } catch(IOException e) {
+        e.printStackTrace();
+      }
     }
   }
-  
+
+  private class Feedback implements Runnable {
+    private int replyPort;
+    private ExecutorService replyExe;
+    private boolean isAlive;
+
+    public Feedback(int replyPort) {
+      this.replyPort = replyPort;
+      this.replyExe = Executors.newCachedThreadPool();
+      this.isAlive = true;
+    }
+
+    public void run() {
+      ServerSocket replySock;
+      try {
+
+        replySock = new ServerSocket(this.replyPort, 200);
+        while(this.isAlive) {
+          this.replyExe.execute(new ReplyHandler(replySock.accept()));
+        }
+        shutdownService(replyExe);
+        replySock.close();
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public void terminate() {
+      this.isAlive = false;
+    }
+  }
+
+  private class ReplyHandler implements Runnable {
+    Socket sock;
+    Gson gson;
+
+    public ReplyHandler(Socket sock) {
+      this.sock = sock;
+      this.gson = new Gson();
+    }
+
+    public void run() {
+      try {
+        BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        String str;
+        while((str = in.readLine()) != null) {
+          if (str.length() == 0) {
+            System.out.println("end of stream");
+          } else if (str.length() < 7) {
+            System.out.println(str + " is not a valid operation");
+          } else {
+            //TODO: error handling
+            str = "{"+ str.split("\\{", 2)[1];
+            //de-serialize json string and handle operation
+
+            Reply reply = gson.fromJson(str, Reply.class);
+            // check if any clientSock match in waiting HashMap
+            int seq = reply.getSeq();
+            synchronized(Replicator.waiting) {
+              // retrieve clientsock and send back reply
+              Socket clientSock = Replicator.waiting.remove(seq);
+              if(clientSock == null) {
+                System.err.println("seq: " + seq + " found with no matching client sock");
+                return;
+              }
+              ObjectOutputStream out = new ObjectOutputStream(clientSock.getOutputStream());
+              out.writeObject(str);
+              out.close();
+              clientSock.close();
+            }
+          }
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  } 
 }
