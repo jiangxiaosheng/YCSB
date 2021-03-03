7e30e20991f25ac052fc8e0b6b2f9e1019fa0fd1
cc4351 <cc4351@node2.cc4351-qv92897.e6897-pg0.utah.cloudlab.us>
2021-03-03 05:42:25 -0700

    build node update

--- replicator/build-node.sh
+++ replicator/build-node.sh
@@ -1,3 +1,3 @@
 rm -rf rocksdb.dir
 javac -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Node.java
-java -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Node
+java -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Node false 128.110.153.219 3456 2345
