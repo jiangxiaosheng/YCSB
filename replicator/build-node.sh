40f0521b174227e84d8cc7efbb17183ec9f29b4a
cc4351 <cc4351@node2.cc4351-qv93168.e6897-pg0.utah.cloudlab.us>
2021-03-04 05:43:02 -0700

    change shell script port number

--- replicator/build-node.sh
+++ replicator/build-node.sh
@@ -1,3 +1,3 @@
 rm -rf rocksdb.dir
 javac -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Node.java
-java -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Node false 128.110.153.219 3456 2345
+java -classpath ../core/target/classes/:../rocksdb/target/dependency/*:. Node true 128.110.153.80 9876 2345
