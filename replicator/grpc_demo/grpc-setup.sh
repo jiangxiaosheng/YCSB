85b8888751f970acc21569555cfad4f8607b1dd8
cc4351 <cc4351@node1.cc4351-qv93523.e6897-pg0.utah.cloudlab.us>
2021-03-09 01:39:01 -0700

    env setup for grpc

---
+++ replicator/grpc_demo/grpc-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/1.36.0/protoc-gen-grpc-java-1.36.0-linux-x86_64.exe
+mv protoc-gen-grpc-java-1.36.0-linux-x86_64.exe protoc-gen-grpc-java
+chmod +x protoc-gen-grpc-java
