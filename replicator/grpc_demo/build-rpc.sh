d0524e2c0a57b3cdd4b108a841a7b348335ddfa1
cc4351 <cc4351@node1.cc4351-qv93523.e6897-pg0.utah.cloudlab.us>
2021-03-09 01:38:24 -0700

    update build script

--- replicator/grpc_demo/build-rpc.sh
+++ replicator/grpc_demo/build-rpc.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-wget https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/1.36.0/protoc-gen-grpc-java-1.36.0-linux-x86_64.exe
-mv protoc-gen-grpc-java/1.36.0/protoc-gen-grpc-java-1.36.0-linux-x86_64.exe protoc-gen-grpc-java/1.36.0/protoc-gen-grpc-java
-chmod +x protoc-gen-grpc-java
-protoc --plugin=protoc-gen-grpc-java --grpc-java_out=. --proto_path=. *.proto
-# protoc --proto_path=./include/google/protobuf/ --java_out=. *.proto
-# javac -classpath ../:../../core/target/classes/:../../rocksdb/target/dependency/*:. HelloWorldClient.java
+if [[ "$#" -eq "0" ]]
+    then echo "run with cmd: ./build-rpc.sh filename-of-proto-without-.proto"
+    exit
+fi
+mkdir -p target/
+./protoc --plugin=protoc-gen-grpc-java --grpc-java_out=target --proto_path=. $1.proto
+./protoc --proto_path=. --java_out=target/ $1.proto
+cd target
+javac -classpath ../../:../../../core/target/classes/:../../../rocksdb/target/dependency/*:. ../*.java
+# javac -classpath ../:../../core/target/classes/:../../rocksdb/target/dependency/*:./target/*:. HelloWorldClient.java
 # java -classpath ../:../../core/target/classes/:../../rocksdb/target/dependency/*:. TestRep 128.110.155.5 1234
