4f4e2b784b369a582b58f23ce4a28f703fb07709
cc4351 <cc4351@node-0.cc4351-qv93696.lsm-rep-pg0.apt.emulab.net>
2021-03-12 01:14:19 -0700

    add client compilation script

--- replicator/grpc_demo/client.sh
+++ replicator/grpc_demo/client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd target
-javac -classpath ../../:../../../core/target/classes/:../../../rocksdb/target/dependency/*:. ../Upstream.java
-java -classpath ../../:../../../core/target/classes/:../../../rocksdb/target/dependency/*:. ../Upstream.java
+cd target/
+YCSB_HOME=/users/$USER/YCSB
+javac -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.: $1
+java -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.: $1
