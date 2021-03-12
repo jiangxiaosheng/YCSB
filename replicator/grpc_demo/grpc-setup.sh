0c9f0d15c533f2b7219c024e69fd0014ff4fb0b2
cc4351 <cc4351@node-0.cc4351-qv93696.lsm-rep-pg0.apt.emulab.net>
2021-03-12 01:11:12 -0700

    update grpc setup script to include proto file

--- replicator/grpc_demo/grpc-setup.sh
+++ replicator/grpc_demo/grpc-setup.sh
@@ -2,3 +2,4 @@
 wget https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/1.36.0/protoc-gen-grpc-java-1.36.0-linux-x86_64.exe
 mv protoc-gen-grpc-java-1.36.0-linux-x86_64.exe protoc-gen-grpc-java
 chmod +x protoc-gen-grpc-java
+wget https://raw.githubusercontent.com/camelboat/EECS_6897_Distributed_Storage_System_Project_Scripts/rubble/rubble/protos/rubble_kv_store.proto
