f2948adacad2a182f7263173eaf5408f285ed751
root <root@node-1.cc4351-qv93696.lsm-rep-pg0.apt.emulab.net>
2021-03-12 03:07:39 -0700

    another update to client build

--- replicator/grpc_demo/client.sh
+++ replicator/grpc_demo/client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd target/
-YCSB_HOME=/users/$USER/YCSB
-javac -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.: $1
+YCSB_HOME=/mnt/sdb/YCSB
+javac -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.: $1.java
 java -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.: $1
