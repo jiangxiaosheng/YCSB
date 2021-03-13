f27a501a89b22a26e4fd34112732a2230ba758c4
cc4351 <cc4351@columbia.edu>
2021-03-13 08:12:38 -0700

    update build script

--- replicator/build-rocksdb.sh
+++ replicator/build-rocksdb.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+cd /mnt/sdb
 git clone https://github.com/camelboat/my_rocksdb.git
+git clone https://github.com/camelboat/EECS_6897_Distributed_Storage_System_Project_Scripts.git
 cd my_rocksdb && git checkout rubble
 cd ..
 wget https://raw.githubusercontent.com/camelboat/EECS_6897_Distributed_Storage_System_Project_Scripts/rubble/setup_scripts/gRPC/grpc_setup.sh
+wget https://raw.githubusercontent.com/camelboat/EECS_6897_Distributed_Storage_System_Project_Scripts/rubble/setup_scripts/gRPC/cmake_install.sh
 bash grpc_setup.sh
-export PATH=/root:$PATH
 apt get install libgflags-dev
+export PATH=/root:/root/bin:$PATH
 cd /mnt/sdb/my_rocksdb/nlohmann_json/json
 git clone https://github.com/nlohmann/json.git
 mv json/* .
