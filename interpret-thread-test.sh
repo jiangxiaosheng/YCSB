5843d975f0d9f8c912b296f55f58bb9b126742a7
cc4351 <cc4351@columbia.edu>
2021-03-17 10:53:11 -0600

    update shell script for build and testing

--- interpret-thread-test.sh
+++ interpret-thread-test.sh
@@ -3,12 +3,17 @@ i=1
 while [ $i -lt 512 ] 
 do
   echo $i "threads -------"
-  if [ $i -lt 3 ]
-  then
-    grep -m $i "latency" thread-test-$i.txt
-  else
-    grep -m 3 "latency" thread-test-$i.txt
-  fi
+  # grep -m 1 "long tail latency" thread-test-$i.txt
+  # echo "long tail count: " 
+  # grep -c "long tail latency" thread-test-$i.txt
+  # if [ $i -lt 3 ]
+  # then
+    # grep -m $i "latency" thread-test-$i.txt
+  # else
+    # grep -m 3 "latency" thread-test-$i.txt
+ #  fi
+  # echo "throughput"
+  grep "Throughput" thread-test-$i.txt
   i=$((i*2))
 done
   
