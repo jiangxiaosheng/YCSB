9cba58cfadf1c5cc1e57905ecb88f709c960eb62
root <root@node0.cc4351-qv94273.lsm-rep-pg0.utah.cloudlab.us>
2021-03-25 11:38:17 -0600

    java replicator update

--- replicator/build-rpc.sh
+++ replicator/build-rpc.sh
@@ -5,7 +5,7 @@ if [[ "$#" -eq "0" ]]
 fi
 mkdir -p target/
 ./protoc --plugin=protoc-gen-grpc-java --grpc-java_out=target --proto_path=. $1
-./protoc --proto_path=. --java_out=target/ $1
+./protoc --proto_path=. --java_out=target/ --cpp_out=cpp_target/ $1
 cd target
 # javac -classpath ../../:../../../core/target/classes/:../../../rocksdb/target/dependency/*:. ../*.java
 # javac -classpath ../:../../core/target/classes/:../../rocksdb/target/dependency/*:./target/*:. HelloWorldClient.java
