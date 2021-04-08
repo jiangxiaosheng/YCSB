2bbd9c7ade2f482c317b1bd329c27a7acb3121ac
Changxu Luo <changxu.luo@columbia.edu>
2021-04-07 22:51:21 -0600

    Add dependency for replicator library

--- replicator/compile.sh
+++ replicator/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+set -ex
+
 cd target/
 YCSB_HOME=/mnt/sdb/YCSB
-javac -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.: $1.java
-java -ea -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:.:  $1
+
+javac -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.: $1.java
+java -ea -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.:  $1
