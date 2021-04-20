15dafcceb241f4f9fb0f2df78be14106eec56ed4
Changxu Luo <changxu.luo@columbia.edu>
2021-04-20 17:53:39 -0600

    Echo replicator PID to proc_table for later killing from remote operator

--- replicator/compile.sh
+++ replicator/compile.sh
@@ -10,3 +10,9 @@ YCSB_HOME=/mnt/sdb/YCSB
 
 javac -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.: $1.java
 java -ea -classpath $YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.:  $1
+
+REPLICATOR_PID=$!
+echo ${REPLICATOR_PID}
+
+mkdir -p /tmp/rubble_proc/
+echo "REPLICATOR ${REPLICATOR_PID}" >> /tmp/rubble_proc/proc_table
