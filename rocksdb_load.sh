e3f98c5002bb9301f1fef19394e789649d11fd9b
axe <xz2872@columbia.edu>
2021-04-27 15:51:13 -0600

    load.sh modified

--- rocksdb_load.sh
+++ rocksdb_load.sh
@@ -22,6 +22,9 @@ echo "remove or touch output file"
 remove_or_touch $LOAD_OUT_FILE
 remove_or_touch $TOP_OUT_FILE
 
+#trim ssd
+fstrim -v /
+
 # Writes data buffered in memory out to disk, then clear memory cache(page cache).
 sudo -S sync; echo 1 | sudo tee /proc/sys/vm/drop_caches
 
@@ -33,18 +36,11 @@ sudo -S sync; echo 1 | sudo tee /proc/sys/vm/drop_caches
 -threads 8 \
 -p hdrhistogram.percentiles=5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,99,99.9 \
 | tee $LOAD_OUT_FILE; } &
-# echo "-----------------------------------" 
-# { top -b -n 1 | grep "Cpu(s)"} 
-# echo "-----------------------------------" 
-# { top -b -n 1 | grep java } &
-# echo "-----------------------------------" 
-# pid=$(pgrep -u root -d , java)
 
-# echo "java pid : ${pid}"
-{ top -b -d 0.2 -o +%CPU \
-| sed -n '8,10p' \
-| awk '{printf "%6s  %-4s  %-4s  %-s\n",$1,$2,$9,$NF}' \
-| grep java | tee $TOP_OUT_FILE; } &
+
+{ top -u root -b -d 0.2 -o +%CPU -w 512 | grep "java" --line-buffered >> $TOP_OUT_FILE; } &
 wait -n
 
+echo -n "CPU Usage : "
+awk '{ total += $9 } END { print total/NR }' $TOP_OUT_FILE
 kill 0
