0d9b72453735a4b222718188e3fc71f622cc3d52
cc4351 <cc4351@columbia.edu>
2021-05-19 22:11:58 -0600

    re-organize replicator compilation

--- replicator/build-ycsb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run this script in sudo mode
-mkdir -p /mnt/sdb && cd /mnt/sdb
-apt update
-apt install default-jdk maven -y
-git clone https://github.com/cc4351/YCSB.git
-cd YCSB
-git checkout singleOp
-./build.sh
--- replicator/compile.sh
+++ replicator/compile.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 set -ex
-
+mkdir -p ./target/rubblejava
 cp Replicator.java ./target/rubblejava/
 mkdir -p ./target/config/
 cp test.yml ./target/config/
 
 cd target/
 YCSB_HOME=../../
-DEPENDENCIES=$YCSB_HOME/core/target/classes/rubblejava/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.:
-javac -classpath ${DEPENDENCIES} $1.java
-java -ea -classpath ${DEPENDENCIES} $1
+TARGET='rubblejava/Replicator'
+DEPENDENCIES=$YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.:
+
+javac -classpath ${DEPENDENCIES} ${TARGET}.java
+java -ea -classpath ${DEPENDENCIES} ${TARGET}
 
 REPLICATOR_PID=$!
 echo ${REPLICATOR_PID}
