1e6f157870f4687182a951e7d384f73c5c9c82a9
cc4351 <cc4351@columbia.edu>
2021-05-25 17:03:34 -0600

    auto-kill previous process

--- replicator/compile.sh
+++ replicator/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+# set -ex
 mkdir -p ./target/rubblejava
 cp Replicator.java ./target/rubblejava/
 mkdir -p ./target/config/
@@ -11,11 +11,16 @@ YCSB_HOME=../../
 TARGET='rubblejava/Replicator'
 DEPENDENCIES=$YCSB_HOME/core/target/classes/:$YCSB_HOME/rocksdb/target/dependency/*:$YCSB_HOME/replicator/lib/*:.:
 
-javac -classpath ${DEPENDENCIES} ${TARGET}.java
-java -ea -classpath ${DEPENDENCIES} ${TARGET}
+{
+    # kill pre-existing Replicator processes before starting a new one
+    kill $(ps aux | grep Replicator | awk '{print $2}')
+} || {
+    javac -classpath ${DEPENDENCIES} ${TARGET}.java
+    java -ea -classpath ${DEPENDENCIES} ${TARGET}
 
-REPLICATOR_PID=$!
-echo ${REPLICATOR_PID}
+    REPLICATOR_PID=$!
+    echo ${REPLICATOR_PID}
 
-mkdir -p /tmp/rubble_proc/
-echo "REPLICATOR ${REPLICATOR_PID}" >> /tmp/rubble_proc/proc_table
+    mkdir -p /tmp/rubble_proc/
+    echo "REPLICATOR ${REPLICATOR_PID}" >> /tmp/rubble_proc/proc_table
+}
